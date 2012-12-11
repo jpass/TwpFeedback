71a9ee1ec11cd20cabf6acd615eed0c04f73a08f
jpass <suchomski1@gmail.com>
2012-12-11 20:34:48 +0100

    [dashboard] added idea form

--- src/Twp/Bundle/DashboardBundle/Controller/DefaultController.php
+++ src/Twp/Bundle/DashboardBundle/Controller/DefaultController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Twp\Entity\Idea;
+
 class DefaultController extends Controller
 {
     /**
@@ -14,6 +16,17 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return array();
+        $idea = new Idea();
+        $form = $this->createFormBuilder($idea)
+            ->add('title', null, array('label' => 'Enter your idea...'))
+            ->add('content', null, array('label' => 'Describe your idea...'))
+            ->add('votes', 'choice', array(
+                'choices' => array(1,2,3),
+                'mapped' => false,
+                'expanded' => true
+                ))
+            ->getForm();
+        
+        return array('form' => $form->createView());
     }
 }
