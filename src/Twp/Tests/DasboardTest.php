797a1c71b286a67b89c7b68756bf209323a24e5c
jpass <suchomski1@gmail.com>
2012-12-13 17:02:04 +0100

    [tests] added few checks on dashboard; added firewall/login-redirect test

--- src/Twp/Tests/DasboardTest.php
+++ src/Twp/Tests/DasboardTest.php
@@ -16,11 +16,16 @@ class MyControllerTest extends WebTestCase
         
         $crawler = $client->request('GET', '/');
         
+        $this->assertTrue($crawler->filter('#new-idea:contains("Add Idea")')->count() > 0);
+        $this->assertTrue($crawler->filter('#new-issue:contains("Add Issue")')->count() > 0);
+        
         $this->assertTrue($crawler->filter('h2:contains("I suggest you...")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Enter your idea...")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Describe your idea...")')->count() > 0);
         
         // check form
-        $this->assertTrue($crawler->filter('#idea_title')->count() > 0);
-        $this->assertTrue($crawler->filter('#idea_content')->count() > 0);
+        $this->assertTrue($crawler->filter('input#idea_title')->count() > 0);
+        $this->assertTrue($crawler->filter('textarea#idea_content')->count() > 0);
         $this->assertTrue($crawler->filter('#idea_votes')->count() > 0);
         $this->assertTrue($crawler->filter('#idea__token')->count() > 0);
         $this->assertTrue($crawler->filter('input[value="Add"]')->count() > 0);
@@ -45,5 +50,22 @@ class MyControllerTest extends WebTestCase
         $this->assertTrue($crawler->filter('.idea .vote > span:contains("Vote")')->count() > 0);
         $this->assertTrue($crawler->filter('.idea .vote > a:contains("Sign in to vote")')->count() > 0);
         
+    }
+    
+    public function testLoginRedirectsToLoginForm()
+    {
+        $this->loadFixtures(array(
+            'Twp\Tests\DataFixtures\ORM\LoadUserData',
+            'Twp\Tests\DataFixtures\ORM\LoadIdeaData'));
+        
+        $client = static::createClient();
+        
+        $client->request('GET', '/idea');
+        
+        $crawler = $client->followRedirect();
+        
+        $this->assertTrue($crawler->filter('input#username')->count() > 0);
+        $this->assertTrue($crawler->filter('input#username')->count() > 0);
+        $this->assertTrue($crawler->filter('button[type="submit"]:contains("login")')->count() > 0);
     }
 }
